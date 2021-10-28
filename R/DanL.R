444e63c5e04f7fe2180e4a36a69913153c8ed923
MarionaMFE <mariona.roigevaliente@mfe.govt.nz>
2021-10-29 09:53:28 +1300

    change DL

--- R/DanL.R
+++ R/DanL.R
@@ -8,49 +8,10 @@
 #' DanL()
 
 
-
-
-DanL <- function(){
-require(magick)
-  tiger <- image_read(normalizePath('inst/Picture1.png'))
-
-  tiger %>%
-    image_scale("350x") %>%
-    image_quantize(128) %>%
-    image_annotate(., "All the best Akika",
-                   size = 11,
-                   color = "black",
-                   location = "+60+250",
-                   font = 'Times') %>%
-
-    image_annotate(., "Thank you for all your meticulous work in DnA / DCM.",
-                   size = 11,
-                   color = "black",
-                   location = "+20+275",
-                   font = 'Times') %>%
-
-    image_annotate(., "I have enjoyed working with you, and learning from you ",
-                   size = 11,
-                   color = "black",
-                   location = "+10+300",
-                   font = 'Times') %>%
-
-    image_annotate(., "on a number of different projects.",
-                   size = 11,
-                   color = "black",
-                   location = "+30+315",
-                   font = 'Times') %>%
-
-    image_annotate(., "I hope you'll continue to solve my many R problems in the user group!!",
-                   size = 11,
-                   color = "black",
-                   location = "+10+340",
-                   font = 'Times') %>%
-
-    image_annotate(., "DanL",
-                   size = 11,
-                   color = "black",
-                   location = "+15+360",
-                   font = 'Times')
-
+DanL <- function()
+{
+  print("All the best Akika")
+  print("Thank you for all your meticulous work in DnA / DCM.")
+  print("I have enjoyed working with you, and learning from you on a number of different projects.")
+  print("I hope you'll continue to solve my many R problems in the user group!!")
 }
